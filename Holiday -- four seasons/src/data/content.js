1b5df390387a9fe1df5cd416527b354376dfaa64
Yuanyuan Mei <101386352+Yuanyuan-Mei@users.noreply.github.com>
2022-09-23 20:43:52 +1000

    Add files via upload

---
+++ Holiday -- four seasons/src/data/content.js
@@ -0,0 +1,27 @@
+const CONTENT_INFO = {
+    island: {
+        h1: "Never Stop",
+        h2: "Explore the world",
+        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis delectus dolores sunt minima id dolorum placeat nemo atque, porro accusamus tempora praesentium! In modi voluptate magni laboriosam est, eveniet molestiae!"
+    },
+    spring: {
+        h1: "Spring",
+        h2: "Feel the new vitality",
+        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, labore commodi sit sequi reprehenderit esse? Ipsam odio fugit libero consequuntur expedita a sint rem, incidunt similique deleniti obcaecati tempora reprehenderit."
+    },
+    summer: {
+        h1: "Summer",
+        h2: "Enjoy passionate time",
+        p: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, culpa quos cumque id adipisci molestiae fugiat pariatur placeat rem eligendi reprehenderit repellendus exercitationem neque, repudiandae harum illum eaque. Pariatur, quia!"
+    },
+    autumn: {
+        h1: "Autumn",
+        h2: "Experience without limits",
+        p: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est optio quis necessitatibus fuga expedita quo dolores praesentium quae eveniet eaque, ad rem. Adipisci dolor impedit quod alias aliquid repellendus sit!"
+    },
+    winter: {
+        h1: "Winter",
+        h2: "Enjoy peace and freedom",
+        p: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus consectetur eligendi sint ratione amet, a dolorum, corrupti illum blanditiis perferendis earum quisquam consequatur rem minus. Aspernatur ad reprehenderit hic eius."
+    }
+}
